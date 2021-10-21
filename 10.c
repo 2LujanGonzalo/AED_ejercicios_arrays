32e3c250e9dd0fe6f7b8f2077546ea8cefe1937b
gonzalo <gonzalodlujan@gmail.com>
2021-10-21 09:43:33 -0300

    diez

--- 10.c
+++ 10.c
@@ -4,7 +4,16 @@
 
 int main(void){
 
-char *truco[]={"Ancho de basto","Ancho de espada","Ancho de oro","Ancho de copa","Dos de basto","Dos de espada","Dos de oro","Dos de copa","Tres de basto","Tres de espada","Tres de oro","Tres de copa","Cuatro de basto","Cuatro de espada","Cuatro de oro","Cuatro de copa","Cinco de basto","Cinco de espada","Cinco de oro","Cinco de copa","Seis de basto","Seis de espada","Seis de oro","Seis de copa","Siete de basto","Siete de espada","Siete de oro","Siete de copa","Diez de basto","Diez de espada","Diez de oro","Diez de copa","Once de basto","Once de espada","Once de oro","Once de copa","Doce de basto","Doce de espada","Doce de oro","Doce de copa"};
-printf("Jugaste la mano y te tocaron: el %s, %s y %s\n",truco[rand()%40],truco[rand()%40],truco[rand()%40] );
+
+char *palo[4]={"Basto","Espada","Oro","Copa"};
+char *numero[10]={"Ancho","Dos","Tres","Cuatro","Cinco","Seis","Siete","Diez","Once","Doce"};
+
+srand(time(NULL));
+
+
+  printf("Te toco el:%s de %s\n", numero[rand()%10],palo[rand()%4]);
+  printf("Te toco el:%s de %s\n", numero[rand()%10],palo[rand()%4]);
+  printf("Te toco el:%s de %s\n", numero[rand()%10],palo[rand()%4]);
+  
   return 0;
 }
