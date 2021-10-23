7e8b550377ef566ee79d03e169484a0c9036eedc
2LujanGonzalo <83368931+2LujanGonzalo@users.noreply.github.com>
2021-10-22 22:09:25 -0300

    Add files via upload

--- 08.c
+++ 08.c
@@ -3,14 +3,18 @@
 
 int main(void) {
 int numeros[6]={1,2,3,4,5,6};
+int temp=numeros[0];
+numeros[0]=numeros[5];
+numeros[5]=temp;
 
-for (int i = 0; i < 6; i++) {
-printf("%d",numeros[i] );
-}
-printf("\n");
-for (int i = 5; i >= 0; i--) {
-  printf("%d",numeros[i] );
-}
-printf("\n" );
+temp=numeros[1];
+numeros[1]=numeros[4];
+numeros[4]=temp;
+
+temp=numeros[2];
+numeros[2]=numeros[3];
+numeros[3]=temp;
+
+printf("%d, %d, %d, %d, %d, %d\n",numeros[0],numeros[1],numeros[2],numeros[3],numeros[4],numeros[5] );
 return 0;
 }
--- 09.c
+++ 09.c
@@ -3,18 +3,50 @@
 
 int main(void) {
 int numeros[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+int temp;
+
+temp=numeros[0];
+numeros[0]=numeros[19];
+numeros[19]=temp;
+
+temp=numeros[1];
+numeros[1]=numeros[18];
+numeros[18]=temp;
+
+temp=numeros[2];
+numeros[2]=numeros[17];
+numeros[17]=temp;
+
+temp=numeros[3];
+numeros[3]=numeros[16];
+numeros[16]=temp;
+
+temp=numeros[4];
+numeros[4]=numeros[15];
+numeros[15]=temp;
+
+temp=numeros[5];
+numeros[5]=numeros[14];
+numeros[14]=temp;
+
+temp=numeros[6];
+numeros[6]=numeros[13];
+numeros[13]=temp;
+
+temp=numeros[7];
+numeros[7]=numeros[12];
+numeros[12]=temp;
+
+temp=numeros[8];
+numeros[8]=numeros[11];
+numeros[11]=temp;
+
+temp=numeros[9];
+numeros[9]=numeros[10];
+numeros[10]=temp;
 
 for (int i = 0; i < 20; i++) {
-printf("%d,",numeros[i] );
+  printf("%d\n",numeros[i]*2 );
 }
-
-printf("\n");
-for (int i = 19; i >= 0; i--) {
-if (numeros[i]%2==0) {
-  printf("%d,",numeros[i] );
-}
-
-}
-printf("\n" );
 return 0;
 }
